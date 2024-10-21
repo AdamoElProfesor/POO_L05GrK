5f27719b2e244082dd1cdb29c611ab33ca8626ce
Adam Kurt Gruber <adam.gruber@heig-vd.ch>
2024-10-21 17:44:28 +0200

    implementation constructor with M,N and ToString method

--- src/main/java/ch/heigvd/poo/Matrix.java
+++ src/main/java/ch/heigvd/poo/Matrix.java
@@ -1,4 +1,5 @@
 package ch.heigvd.poo;
+import java.util.Random;
 
 public class Matrix {
     private int N;
@@ -7,13 +8,32 @@ public class Matrix {
     private int[][] values;
 
     public Matrix( int N, int M, int mod ) {
+        this.N = N;
+        this.M = M;
+        this.mod = mod;
+        this.values = new int[N][M];
+
+        java.util.Random random = new java.util.Random();
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                this.values[i][j] = random.nextInt(mod);
+            }
+        }
     }
     public Matrix(int[][] values, int mod) {
     }
 
     @Override
     public String toString() {
-        return "Method toString from matrix called";
+        String str = new String("");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                str += values[i][j] + " ";
+            }
+            str += "\n";
+        }
+        return str;
     }
 
     public Matrix addition(Matrix other){
