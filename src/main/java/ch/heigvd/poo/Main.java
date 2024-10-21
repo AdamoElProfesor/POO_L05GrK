38d88db2056238939ce2c01da2a0f5252ef6762e
Adam Kurt Gruber <adam.gruber@heig-vd.ch>
2024-10-21 14:37:04 +0200

    skeleton of the class matrix done

---
+++ src/main/java/ch/heigvd/poo/Main.java
@@ -0,0 +1,8 @@
+package ch.heigvd.poo;
+
+public class Main {
+    public static void main(String[] args) {
+        Matrix matrix = new Matrix(3,3,3);
+        System.out.println(matrix);
+    }
+}
